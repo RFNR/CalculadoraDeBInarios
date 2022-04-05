c8d62e7baae71888000b4db111aeae4d9b854618
RFNR <ronald.fernr@gmail.com>
2022-04-05 13:05:05 -0300

    Factoriales

---
+++ factorial.js
@@ -0,0 +1,9 @@
+// Calculadora de factoriales
+
+function factorial(n){
+
+    if(n === 1 || n === 0) return 1;
+    else if (n < 0) return "No existe el factorial de un numero negativo.";
+    return n * factorial(n - 1)
+    
+}
